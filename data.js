537c1a523797b675cf6f7e18770a10f4328caec5
kaiiiiiiiiiiii777 <kairust2025@gmail.com>
2026-03-11 17:57:09 +0000

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,8 @@
-
+{
+    "username": "JohnDoe",
+    "password": "securepass123",
+    "duration": "30 days",
+    "uuid": "123e4567-e89b-12d3-a456-426614174000",
+    "banned": false,
+    "dateStarted": "2024-01-01"
+}
