96a160bafa88319491168bedd02c524ed442aa6e
kaiiiiiiiiiiii777 <kairust2025@gmail.com>
2026-03-11 20:03:44 +0000

    Update credentials for new user

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 'newuser': {
-    password: 'theirpassword',
-    username: 'new_username',
+    password: 'axis',
+    username: 'axis',
     uuid: 'USR-999-2024',
     dateStarted: '2024-06-01',
     duration: '1 day',
